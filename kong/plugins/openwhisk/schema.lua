b5326def4f053388ac8f2b2582a2c61a125ddc9a
Guilherme Salazar <gsz@acm.org>
2019-04-17 11:33:52 -0700

    chore(schema) convert to new dao format

--- kong/plugins/openwhisk/schema.lua
+++ kong/plugins/openwhisk/schema.lua
@@ -1,14 +1,20 @@
 return {
+  name = "openwhisk",
   fields = {
-    timeout       = { type = "number",  default  = 60000 },
-    keepalive     = { type = "number",  default  = 60000 },
-    service_token = { type = "string"                    },
-    host          = { type = "string",  required = true  },
-    port          = { type = "number",  default  = 443   },
-    path          = { type = "string",  required = true  },
-    action        = { type = "string",  required = true  },
-    https         = { type = "boolean", default  = true  },
-    https_verify  = { type = "boolean", default  = false },
-    result        = { type = "boolean", default  = true  },
+    config = {
+      type = "record",
+      fields = {
+        { timeout       = { type = "integer", default  = 60000 } },
+        { keepalive     = { type = "integer", default  = 60000 } },
+        { service_token = { type = "string"                     } },
+        { host          = { type = "string",  required = true   } },
+        { port          = { type = "integer", default  = 443   } },
+        { path          = { type = "string",  required = true   } },
+        { action        = { type = "string",  required = true   } },
+        { https         = { type = "boolean", default  = true   } },
+        { https_verify  = { type = "boolean", default  = false  } },
+        { result        = { type = "boolean", default  = true   } },
+      }
+    }
   }
 }
