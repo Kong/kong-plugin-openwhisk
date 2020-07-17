99fc3bebfb6cc54c0042c1fb2c73777f784df24f
Sebastian Saldarriaga <sebastian@ubidots.com>
2020-07-17 01:58:45 -0500

    Fixed field data type issue in the plugin schema

--- kong/plugins/openwhisk/schema.lua
+++ kong/plugins/openwhisk/schema.lua
@@ -1,19 +1,21 @@
 return {
   name = "openwhisk",
   fields = {
-    config = {
-      type = "record",
-      fields = {
-        { timeout       = { type = "integer", default  = 60000 } },
-        { keepalive     = { type = "integer", default  = 60000 } },
-        { service_token = { type = "string"                     } },
-        { host          = { type = "string",  required = true   } },
-        { port          = { type = "integer", default  = 443   } },
-        { path          = { type = "string",  required = true   } },
-        { action        = { type = "string",  required = true   } },
-        { https         = { type = "boolean", default  = true   } },
-        { https_verify  = { type = "boolean", default  = false  } },
-        { result        = { type = "boolean", default  = true   } },
+    {
+      config = {
+        type = "record",
+        fields = {
+          { timeout       = { type = "integer", default  = 60000 } },
+          { keepalive     = { type = "integer", default  = 60000 } },
+          { service_token = { type = "string"                     } },
+          { host          = { type = "string",  required = true   } },
+          { port          = { type = "integer", default  = 443   } },
+          { path          = { type = "string",  required = true   } },
+          { action        = { type = "string",  required = true   } },
+          { https         = { type = "boolean", default  = true   } },
+          { https_verify  = { type = "boolean", default  = false  } },
+          { result        = { type = "boolean", default  = true   } },
+        }
       }
     }
   }
