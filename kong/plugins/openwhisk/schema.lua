b25c56dc68d2c4166accfe6afc176a6c48220f90
Thijs Schreijer <thijs@thijsschreijer.nl>
2020-07-20 16:46:28 +0200

    fix(schema) bad schema definition

--- kong/plugins/openwhisk/schema.lua
+++ kong/plugins/openwhisk/schema.lua
@@ -1,19 +1,23 @@
+local typedefs = require "kong.db.schema.typedefs"
+
 return {
   name = "openwhisk",
   fields = {
-    config = {
-      type = "record",
-      fields = {
-        { timeout       = { type = "integer", default  = 60000 } },
-        { keepalive     = { type = "integer", default  = 60000 } },
-        { service_token = { type = "string"                     } },
-        { host          = { type = "string",  required = true   } },
-        { port          = { type = "integer", default  = 443   } },
-        { path          = { type = "string",  required = true   } },
-        { action        = { type = "string",  required = true   } },
-        { https         = { type = "boolean", default  = true   } },
-        { https_verify  = { type = "boolean", default  = false  } },
-        { result        = { type = "boolean", default  = true   } },
+    { protocols = typedefs.protocols_http },
+    { config = {
+        type = "record",
+        fields = {
+          { timeout       = { type = "integer", default  = 60000 } },
+          { keepalive     = { type = "integer", default  = 60000 } },
+          { service_token = { type = "string"                     } },
+          { host          = typedefs.host({ required = true }), },
+          { port          = typedefs.port({ required = true, default = 443 }), },
+          { path          = { type = "string",  required = true   } },
+          { action        = { type = "string",  required = true   } },
+          { https         = { type = "boolean", default  = true   } },
+          { https_verify  = { type = "boolean", default  = false  } },
+          { result        = { type = "boolean", default  = true   } },
+        }
       }
     }
   }
