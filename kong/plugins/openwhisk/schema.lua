12352c472d5033042afc25c7eb812a7118b1fc04
Sebastian Saldarriaga <sebastian@ubidots.com>
2020-08-10 03:01:40 -0500

    Allowed origins
    Added support for validating the origin request to allow the user to improve the security of their functions URLs

--- kong/plugins/openwhisk/schema.lua
+++ kong/plugins/openwhisk/schema.lua
@@ -1,7 +1,5 @@
 local typedefs = require "kong.db.schema.typedefs"
 
-local _MAP_VALUES_TYPES = { "boolean", "set", "array", "string", "integer", "number", "map" }
-
 return {
   name = "openwhisk",
   fields = {
@@ -9,24 +7,32 @@ return {
     { config = {
         type = "record",
         fields = {
-          { timeout       = { type = "integer", default  = 60000 } },
-          { keepalive     = { type = "integer", default  = 60000 } },
-          { service_token = { type = "string"                     } },
-          { host          = typedefs.host({ required = true }), },
-          { port          = typedefs.port({ required = true, default = 443 }), },
-          { path          = { type = "string",  required = true   } },
-          { action        = { type = "string",  required = true   } },
-          { https         = { type = "boolean", default  = true   } },
-          { https_verify  = { type = "boolean", default  = false  } },
-          { result        = { type = "boolean", default  = true   } },
-          { environment   = { type = "string",  default  = "{}"   } },
-          { parameters    = { type = "string",  default  = "{}"   } },
-          { methods        = {
+          { timeout         = { type = "integer", default  = 60000 } },
+          { keepalive       = { type = "integer", default  = 60000 } },
+          { service_token   = { type = "string"                     } },
+          { host            = typedefs.host({ required = true }), },
+          { port            = typedefs.port({ required = true, default = 443 }), },
+          { path            = { type = "string",  required = true   } },
+          { action          = { type = "string",  required = true   } },
+          { https           = { type = "boolean", default  = true   } },
+          { https_verify    = { type = "boolean", default  = false  } },
+          { result          = { type = "boolean", default  = true   } },
+          { environment     = { type = "string",  default  = "{}"   } },
+          { parameters      = { type = "string",  default  = "{}"   } },
+          { methods         = {
               type = "array",
               default  = { "POST" },
               elements = {
                 type = "string",
                 one_of = { "HEAD", "GET", "POST", "PATCH", "PUT" }
+              }
+            }
+          },
+          { allowed_origins = {
+              type = "array",
+              default  = {},
+              elements = {
+                type = "string",
               }
             }
           },
